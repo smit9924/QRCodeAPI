8d8445ede794ccc5757fb3904069774cf68b1721
smitPatel1302 <smit.dpatel9924@gmail.com>
2022-10-24 00:38:19 +0530

    final changes

--- QRCodeAPI/settings.py
+++ QRCodeAPI/settings.py
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'whitenoise.runserver_nostatic',
-    'django.contrib.staticfiles',
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # 'QRGenerate'
@@ -122,9 +121,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR , "static")
-# ]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR , "static")
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
